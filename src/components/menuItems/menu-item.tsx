b2ca5964f3aedc62148fae2309758052137d3c63
harish <harishgarg.tech@gmail.com>
2025-08-22 11:18:51 +0530

    feat: Enhance BasicMenu component with improved styling and layout; add CSS module for better organization

--- src/components/menuItems/menu-item.tsx
+++ src/components/menuItems/menu-item.tsx
@@ -5,9 +5,10 @@ import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import { signOut } from "next-auth/react";
-import { ListItemIcon, ListItemText, Tooltip } from "@mui/material";
+import { Box, ListItemIcon, ListItemText, Paper, Tooltip } from "@mui/material";
 import LogoutIcon from "@mui/icons-material/Logout";
 import { useSession } from "next-auth/react";
+import style from './page.module.css'
 
 export default function BasicMenu() {
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
@@ -19,6 +20,8 @@ export default function BasicMenu() {
     setAnchorEl(null);
   };
 
+
+
   let userName = "";
   let email = "";
   const { data: session, status } = useSession();
@@ -29,8 +32,11 @@ export default function BasicMenu() {
     userName = session.user?.name || "Guest";
     email = session.user?.email || "guest@example.com";
   }
+
+
+
   return (
-    <div>
+    <Box  >
       <Button
         id="basic-button"
         aria-controls={open ? "basic-menu" : undefined}
@@ -40,11 +46,9 @@ export default function BasicMenu() {
       >
         <KeyboardArrowDownIcon sx={{ color: "white" }} />
       </Button>
+
       <Menu
-        sx={{
-          maxWidth: "240px",
-          minHeight: "149px",
-        }}
+        className={style.profileDropdown}
         id="basic-menu"
         anchorEl={anchorEl}
         open={open}
@@ -55,37 +59,31 @@ export default function BasicMenu() {
           },
         }}
       >
-        <MenuItem onClick={handleClose}>
-          <Tooltip title={email} placement="bottom-start">
-            <ListItemText
-              sx={{
-                "& .MuiListItemText-primary": {
-                  display: "block",
-                  overflow: "hidden",
-                  textOverflow: "ellipsis",
-                  whiteSpace: "nowrap",
-                  maxWidth: "180px",
-                },
-                "& .MuiListItemText-secondary": {
-                  display: "block",
-                  overflow: "hidden",
-                  textOverflow: "ellipsis",
-                  whiteSpace: "nowrap",
-                  maxWidth: "180px",
-                },
-              }}
-              primary={userName}
-              secondary={email}
-            />
+        <MenuItem className={style.mainContainer} onClick={handleClose}>
+
+           <Box  className={style.userData}>
+              <Box className={style.listItemInnerBox}>
+              
+             <Box component='p' className={style.primaryText}>{userName}</Box>
+                      <Tooltip className={style.tooltip} title={email} placement="bottom-start">
+
+             <Box component='p' className={style.secondaryText}>{email}</Box>
           </Tooltip>
+            </Box>
+           </Box>
         </MenuItem>
-        <MenuItem onClick={() => signOut({ callbackUrl: "/login" })}>
-          <ListItemIcon>
-            <LogoutIcon fontSize="small" />
+        <MenuItem className={style.logoutButton} onClick={() => signOut({ callbackUrl: "/login" })}>
+        <Box className={style.logoutBox}>
+          <ListItemIcon className={style.logoutIcon}>
+            <LogoutIcon  sx={{ color: "#424242",height:'20px', width:'20px' }} />
           </ListItemIcon>
-          Logout
+          <Box className={style.logoutText}>
+            Logout
+          </Box>
+        </Box>
         </MenuItem>
       </Menu>
-    </div>
+
+    </Box>
   );
 }
